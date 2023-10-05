68973867a0438f632a4347ba076fb0f887a637a3
Tom <mcreyntm@mail.uc.edu>
2023-10-04 21:03:39 -0400

    Added FirestoreHelper class for adding user data into database

---
+++ app/src/main/java/com/protify/protifyapp/FireStoreHelper.kt
@@ -0,0 +1,48 @@
+package com.protify.protifyapp
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
+
+class FireStoreHelper {
+    private val db = Firebase.firestore
+    init {
+        // [START initialize_firestore]
+        val db = Firebase.firestore
+        // [END initialize_firestore]
+    }
+
+
+
+    @OptIn(ExperimentalTime::class)
+    fun userExists(uid: String, dateCreated: Long) {
+        // [START get_document]
+        val docRef = db.collection("users").document(uid)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    //Add function here
+                } else {
+                    val seconds = dateCreated.seconds.inWholeSeconds
+                    val nanoseconds = dateCreated.nanoseconds.inWholeNanoseconds.toInt()
+                    createUserDocument(uid, dateCreated)
+                }
+            }
+    }
+    private fun createUserDocument(uid: String, dateCreated: Long) {
+        // [START add_document]
+        val user = hashMapOf(
+            "dateCreated" to dateCreated,
+            "uid" to uid,
+        )
+        db.collection("users").document(uid)
+            .set(user)
+            .addOnSuccessListener { documentReference ->
+                //Add function here
+            }
+            .addOnFailureListener { e ->
+            }
+    }
+}
