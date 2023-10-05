3ef503ce86937cc1677645c6bfe7dd9f91a6af76
Tom <mcreyntm@mail.uc.edu>
2023-10-04 21:04:09 -0400

    Call FirestoreHelper function to add user info to database if it doesn't exist

--- app/src/main/java/com/protify/protifyapp/HomeActivity.kt
+++ app/src/main/java/com/protify/protifyapp/HomeActivity.kt
@@ -3,12 +3,18 @@ package com.protify.protifyapp
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+
 class HomeActivity {
     @Composable
-    fun HomePage( navController: NavController) {
+    fun HomePage(navController: NavController) {
         //Get the current user from the EmailPasswordActivity class
         val user = EmailPasswordActivity().getCurrentUser()
         //Display the user's email
         Text(text = "Welcome ${user?.email}")
+        if (user != null) {
+            //Get the user's information from the database
+
+            FireStoreHelper().userExists(user.uid, user.metadata!!.creationTimestamp)
         }
     }
+}
