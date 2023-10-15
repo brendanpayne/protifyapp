22e7ee4c245d0f9bc1dfc1ff61822892e3f7cc09
Tom <mcreyntm@mail.uc.edu>
2023-10-15 13:05:29 -0400

    Added network change listener

--- app/src/main/java/com/protify/protifyapp/NetworkManager.kt
+++ app/src/main/java/com/protify/protifyapp/NetworkManager.kt
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
+import android.util.Log
 
 class NetworkManager(private val context:Context) {
 
@@ -13,30 +14,28 @@ class NetworkManager(private val context:Context) {
             .build()
 
         private var isConnected = false
+        private var connectivityChangeCallback: ((Boolean) -> Unit)? = null
+    fun setNetworkChangeListener(listener: (Boolean) -> Unit) {
+        connectivityChangeCallback = listener
+    }
 
         private val networkCallback = object : ConnectivityManager.NetworkCallback() {
             override fun onAvailable(network: android.net.Network) {
                 super.onAvailable(network)
+                Log.d("NetworkManager", "Network is available")
                 isConnected = true
+                connectivityChangeCallback?.invoke(true)
             }
 
             override fun onLost(network: Network) {
                 super.onLost(network)
+                Log.d("NetworkManager", "Network is lost")
                 isConnected = false
+                connectivityChangeCallback?.invoke(false)
             }
         }
-    init {
-        // Starts listening by default
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
-
-        val activeNetworkInfo = connectivityManager.activeNetwork
-
-        val capabilities = connectivityManager.getNetworkCapabilities(activeNetworkInfo)
-        isConnected = capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
-    }
     fun startListening() {
+
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
