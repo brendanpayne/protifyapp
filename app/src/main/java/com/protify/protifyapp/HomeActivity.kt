d555b9f0cf4e275a25d0989aa37718b20432d0d4
Tom <mcreyntm@mail.uc.edu>
2023-10-21 11:51:03 -0400

    LaunchedEffect checks constantly for network connection

--- app/src/main/java/com/protify/protifyapp/HomeActivity.kt
+++ app/src/main/java/com/protify/protifyapp/HomeActivity.kt
@@ -2,6 +2,11 @@ package com.protify.protifyapp
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavController
 import com.protify.protifyapp.features.login.FirebaseLoginHelper
 import java.time.LocalDateTime
@@ -10,18 +15,28 @@ class HomeActivity {
     @Composable
     fun HomePage(navController: NavController) {
         //Get the current user from the EmailPasswordActivity class
+        var firestoreHelper = FirestoreHelper()
         val user = FirebaseLoginHelper().getCurrentUser()
         //Display the user's email
         Text(text = "Welcome ${user?.email}")
 
         var networkManager = NetworkManager(navController.context)
-        networkManager.startListening()
-        var test = networkManager.isConnected()
-        if (user != null) {
-            //Get the user's information from the database
-            FirestoreHelper().toggleOfflineOnline(networkManager.isConnected()) {hasRan ->
-                if (hasRan) {
-                    FirestoreHelper().userExists(user.uid, user.metadata!!.creationTimestamp) {userExists ->
+
+        var isConnected by remember { mutableStateOf(false) }
+        LaunchedEffect(networkManager) {
+            networkManager.startListening()
+        }
+        LaunchedEffect(isConnected) {
+            networkManager.setNetworkChangeListener {
+                if(it) {
+                    firestoreHelper.toggleOfflineOnline(true)
+                } else {
+                    firestoreHelper.toggleOfflineOnline(false)
+                }
+
+            }
+        }
+                    FirestoreHelper().userExists(user!!.uid, user.metadata!!.creationTimestamp) {userExists ->
                         if (userExists) {
                             FirestoreHelper().addEvent(user.uid, FirestoreEvent(
                                 name = "Test Event",
@@ -36,9 +51,5 @@ class HomeActivity {
 
                         }
                     }
-                }
             }
-
-        }
     }
-}
