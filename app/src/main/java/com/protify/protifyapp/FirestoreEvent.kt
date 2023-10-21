0b1851e9ec0179cce4dc52f22549bc3356ad6924
Tom <mcreyntm@mail.uc.edu>
2023-10-21 11:51:47 -0400

    Still need to implement, but added event validation

--- app/src/main/java/com/protify/protifyapp/FirestoreEvent.kt
+++ app/src/main/java/com/protify/protifyapp/FirestoreEvent.kt
@@ -13,4 +13,31 @@ data class FirestoreEvent(
     val importance: Int?,
     val attendees: Attendees?
 
+
+
 )
+fun validateEvent(event: FirestoreEvent): List<Error> {
+    val errors = mutableListOf<Error>()
+    validateName(event.name)?.let { errors.add(it) }
+    event.description?.let { validateDescription(it)?.let { errors.add(it) } }
+    event.importance?.let { validateImportance(it)?.let { errors.add(it) } }
+    return errors
+}
+private fun validateName(name: String): Error? {
+    if (name.length > 50) {
+        return Error("Name is too long")
+    }
+    return null
+}
+private fun validateDescription(description: String): Error? {
+    if (description.length > 250) {
+        return Error("Description is too long")
+    }
+    return null
+}
+private fun validateImportance(importance: Int): Error? {
+    if (importance < 1 || importance > 5) {
+        return Error("Importance must be between 1 and 5")
+    }
+    return null
+}
