23c18a1fd38ba0d02521b31a1ebbf0ff2c2828aa
brendanpayne <payneb2@mail.uc.edu>
2023-11-01 18:12:44 -0400

    created first functional compose unit test

--- app/src/main/java/com/protify/protifyapp/MainActivity.kt
+++ app/src/main/java/com/protify/protifyapp/MainActivity.kt
@@ -3,20 +3,9 @@ package com.protify.protifyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.protify.protifyapp.features.login.LoginActivity
-import com.protify.protifyapp.features.login.RegisterActivity
 import com.protify.protifyapp.ui.theme.ProtifyTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,44 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ProtifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    //Greeting("Android")
-                    val navController = rememberNavController()
-                   NavHost(navController = navController, startDestination = "main") {
-                          composable("main") {
-                              Row {
-                                  Button(onClick = { navController.navigate("register") }) {
-                                      Text("Register")
-                                  }
-                                  Button(onClick = { navController.navigate("login") }) {
-                                      Text("Login")
-                                  }
-                              }
-                          }
-                       composable("register") {
-                           RegisterActivity().LandingPage() {
-                               LoginActivity().navigateToHomePage(
-                                   navController = navController
-                               )
-                           }
-                   }
-                       composable("login") {
-                        LoginActivity().LoginPage {
-                            LoginActivity().navigateToHomePage(
-                                navController = navController
-                            )
-                        }
-                       }
-                       composable("home") {
-                           HomeActivity().HomePage(navController = navController)
-                       }
-                   }
-                }
-
+                AccountActivity().AccountPage()
             }
         }
     }
--- app/src/main/java/com/protify/protifyapp/features/login/LoginActivity.kt
+++ app/src/main/java/com/protify/protifyapp/features/login/LoginActivity.kt
@@ -1,18 +1,25 @@
 package com.protify.protifyapp.features.login
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.*
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 class LoginActivity {
     private var email by mutableStateOf("")
     private var password by mutableStateOf("")
-    private val firebaseLoginHelper = FirebaseLoginHelper()
+    //private val firebaseLoginHelper = FirebaseLoginHelper()
 
     private fun updateEmail(newEmail: String) {
         email = newEmail
@@ -22,43 +29,70 @@ class LoginActivity {
         password = newPassword
     }
 
+    @OptIn(ExperimentalComposeUiApi::class)
     @Composable
     fun LoginPage(navigateToHomePage: () -> Unit) {
         val context = LocalContext.current
-        Column {
-            TextField(
-                value = email,
-                onValueChange = { email -> updateEmail(email) },
-                label = { Text("Email") }
-            )
-            TextField(
-                value = password,
-                onValueChange = { password -> updatePassword(password) },
-                label = { Text("Password") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-            )
-            Button(onClick = {
-                FirebaseLoginHelper().signIn(email, password).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        task.result?.user?.let {
-                            //navController.navigate("home")
+        val listState = rememberLazyListState()
+        LazyColumn(
+            state = listState,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Text(
+                    text = "Welcome Back!",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+            item {
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email -> updateEmail(email) },
+                    label = { Text("Email") },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next)
+                )
+            }
+            item {
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password -> updatePassword(password) },
+                    label = { Text("Password") },
+                    singleLine = true,
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                )
+            }
+            item {
+                Button(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    onClick = {
+                    FirebaseLoginHelper().signIn(email, password).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            task.result?.user?.let {
+                                //navController.navigate("home")
+                                navigateToHomePage()
+                            }
+                        } else {
+                            Toast.makeText(
+                                context,
+                                "Login Failed",
+                                Toast.LENGTH_SHORT,
+                            ).show()
                             navigateToHomePage()
                         }
-                    } else {
-                       Toast.makeText(
-                           context,
-                           "Login Failed",
-                           Toast.LENGTH_SHORT,
-                       ).show()
-                        navigateToHomePage()
                     }
+                }) {
+                    Text("Login")
                 }
-            }) {
-                Text("Login")
             }
-
         }
 
     }
