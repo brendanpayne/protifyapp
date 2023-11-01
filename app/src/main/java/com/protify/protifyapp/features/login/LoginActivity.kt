3846d4b06ff2588b3ab3c62c06f54e62c8c91b7c
brendanpayne <payneb2@mail.uc.edu>
2023-11-01 13:52:15 -0400

    fixed textfields not updating

--- app/src/main/java/com/protify/protifyapp/features/login/LoginActivity.kt
+++ app/src/main/java/com/protify/protifyapp/features/login/LoginActivity.kt
@@ -2,32 +2,41 @@ package com.protify.protifyapp.features.login
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.*
 import androidx.navigation.NavController
 
 class LoginActivity {
-    var email = "tommy.mcreynolds1@gmail.com"
-    var password = "testpassword"
-    val firebaseLoginHelper = FirebaseLoginHelper()
+    private var email by mutableStateOf("")
+    private var password by mutableStateOf("")
+    private val firebaseLoginHelper = FirebaseLoginHelper()
+
+    private fun updateEmail(newEmail: String) {
+        email = newEmail
+    }
+
+    private fun updatePassword(newPassword: String) {
+        password = newPassword
+    }
+
     @Composable
     fun LoginPage(navigateToHomePage: () -> Unit) {
         val context = LocalContext.current
         Column {
             TextField(
                 value = email,
-                onValueChange = { email = it },
+                onValueChange = { email -> updateEmail(email) },
                 label = { Text("Email") }
             )
             TextField(
                 value = password,
-                onValueChange = { password = it },
+                onValueChange = { password -> updatePassword(password) },
                 label = { Text("Password") },
-                visualTransformation = PasswordVisualTransformation()
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
             )
             Button(onClick = {
                 FirebaseLoginHelper().signIn(email, password).addOnCompleteListener { task ->
