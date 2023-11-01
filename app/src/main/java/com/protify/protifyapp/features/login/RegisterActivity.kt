642f0ffeb044aa660e89180fd7aa4c89e45ec9ea
brendanpayne <payneb2@mail.uc.edu>
2023-11-01 13:52:15 -0400

    fixed textfields not updating

--- app/src/main/java/com/protify/protifyapp/features/login/RegisterActivity.kt
+++ app/src/main/java/com/protify/protifyapp/features/login/RegisterActivity.kt
@@ -3,21 +3,29 @@ package com.protify.protifyapp.features.login
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.lifecycle.ViewModel
 
-class RegisterActivity {
+class RegisterActivity: ViewModel() {
 
-     //var email = "tommy.mcreynolds@gmail.com"
-     //var password = "testpassword"
-    var email = ""
-    var password = ""
-     val firebaseLoginHelper = FirebaseLoginHelper()
+    private var email by mutableStateOf("")
+    private var password by mutableStateOf("")
+    private val firebaseLoginHelper = FirebaseLoginHelper()
 
+    private fun updateEmail(newEmail: String) {
+        email = newEmail
+    }
+
+    private fun updatePassword(newPassword: String) {
+        password = newPassword
+    }
 
     @Composable
     fun LandingPage(navigateToHomePage: () -> Unit) {
@@ -26,14 +34,15 @@ class RegisterActivity {
         Column {
             TextField(
                 value = email,
-                onValueChange = { email = it },
+                onValueChange = { email -> updateEmail(email) },
                 label = { Text("Email") }
             )
             TextField(
                 value = password,
-                onValueChange = { password = it },
+                onValueChange = { password -> updatePassword(password) },
                 label = { Text("Password") },
-                visualTransformation = PasswordVisualTransformation()
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
             )
             Button(onClick = {
                 firebaseLoginHelper.createAccountCallback(email, password) { success, failure ->
@@ -52,4 +61,4 @@ class RegisterActivity {
             }
         }
     }
-    }
+}
