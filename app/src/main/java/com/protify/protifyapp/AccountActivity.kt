abae60d0111e3e381a300029570f112c6db0aeba
Tom <mcreyntm@mail.uc.edu>
2023-11-11 11:04:35 -0500

    Added auto-login

--- app/src/main/java/com/protify/protifyapp/AccountActivity.kt
+++ app/src/main/java/com/protify/protifyapp/AccountActivity.kt
@@ -7,11 +7,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.protify.protifyapp.features.events.AddEvent
+import com.protify.protifyapp.features.login.FirebaseLoginHelper
 import com.protify.protifyapp.features.login.LoginActivity
 import com.protify.protifyapp.features.login.RegisterActivity
 
@@ -24,6 +28,9 @@ class AccountActivity {
         ) {
             //Greeting("Android")
             val navController = rememberNavController()
+            val currentUser by remember {
+                mutableStateOf(FirebaseLoginHelper().getCurrentUser())
+            }
             NavHost(navController = navController, startDestination = "main") {
                 composable("main") {
                     Row {
@@ -63,6 +70,9 @@ class AccountActivity {
                         )
                     }
                 }
+            }
+            if (currentUser != null) {
+                navController.navigate("home")
             }
         }
     }
