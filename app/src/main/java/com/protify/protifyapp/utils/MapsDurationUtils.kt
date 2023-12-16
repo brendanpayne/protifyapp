1d82b4d3e585479960b5ebe769453932afb5c73d
Tom <mcreyntm@mail.uc.edu>
2023-12-15 22:40:45 -0500

    Basic Maps API implementation with data class for storing json

---
+++ app/src/main/java/com/protify/protifyapp/utils/MapsDurationUtils.kt
@@ -0,0 +1,57 @@
+package com.protify.protifyapp.utils
+
+import DistanceResponse
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import java.io.IOException
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+class MapsDurationUtils(startLong: Double, startLat: Double,
+                        // destinationLong: Double, destinationLat: Double,
+                        departTime: LocalDateTime) {
+    val mapsKey = System.getenv("maps_api")
+    val beginningOfTime = LocalDateTime.of(1970, 1, 1, 0, 0, 0)
+    val departTimeFix = departTime.plusDays(1).toEpochSecond(ZoneOffset.UTC) - beginningOfTime.toEpochSecond(ZoneOffset.UTC)
+    val url = "https://maps.googleapis.com/maps/api/distancematrix/json" +
+            "?departure_time=${departTimeFix}" +
+            "&destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626" +
+            "&origins=$startLong%2C$startLat" +
+            "&key=$mapsKey"
+
+    private val client: OkHttpClient = OkHttpClient().newBuilder()
+        .connectTimeout(10, java.util.concurrent.TimeUnit.SECONDS)
+        .readTimeout(10, java.util.concurrent.TimeUnit.SECONDS)
+        .build()
+    private val gson = GsonBuilder().create()
+
+    private fun getDuration(onComplete: (DistanceResponse?) -> Unit) {
+        val request = okhttp3.Request.Builder()
+            .url(url)
+            .build()
+        client.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                onComplete(null)
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                val body = response.body?.string()
+                if (response.code != 200) {
+                    onComplete(null)
+                    return
+                }
+                val distanceResponse = gson.fromJson(body, DistanceResponse::class.java)
+                onComplete(distanceResponse)
+            }
+        })
+    }
+    fun getDistance(onComplete: (Int) -> Unit) {
+        getDuration { distanceResponse ->
+            if (distanceResponse == null) {
+                onComplete(0)
+                return@getDuration
+            }
+            onComplete(distanceResponse.rows[0].elements[0].duration.value)
+        }
+    }
+}
