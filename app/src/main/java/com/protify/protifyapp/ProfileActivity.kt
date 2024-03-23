7fff96be36e8a3b999cb9c4af617232b17725dec
Tom <mcreyntm@mail.uc.edu>
2024-03-23 16:47:46 -0400

    First commit of backend

--- app/src/main/java/com/protify/protifyapp/ProfileActivity.kt
+++ app/src/main/java/com/protify/protifyapp/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.protify.protifyapp
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
@@ -17,22 +19,26 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class ProfileActivity {
     val user = FirebaseAuth.getInstance().currentUser
-    val name = user?.displayName.takeIf { it?.isNotEmpty() == true } ?: "Unknown"
+    var name by mutableStateOf(user?.displayName.takeIf { it?.isNotEmpty() == true } ?: "Enter your Display Name") // Mutable state to recompose when the name changes
     val email = user?.email ?: "Unknown"
+
 
     @Composable
     fun BackButton(navController: NavController) {
@@ -43,8 +49,16 @@ class ProfileActivity {
 
     @Composable
     fun ProfilePage(navController: NavController) {
-        var displayName by remember { mutableStateOf(name) }
-        var homeAddress by remember { mutableStateOf("") }
+        var homeAddress by remember { mutableStateOf("Loading...") } // Mutable state to recompose when the home address changes
+        var newName by remember { mutableStateOf(name) }
+        var newHomeAddress by remember { mutableStateOf("Loading...") }
+        var context = LocalContext.current // Context for showing toasts
+        // Fetch the user's home address from Firestore async so it doesn't block the loading of the page
+        LaunchedEffect(key1 = user) {// This should only run once because user is a stable reference
+            val fetchedResult = FirestoreHelper().getUserHomeAddress(user!!.uid)
+            homeAddress = fetchedResult
+            newHomeAddress = fetchedResult
+        }
 
         Column(
             modifier = Modifier
@@ -71,14 +85,14 @@ class ProfileActivity {
                         .padding(16.dp)
                 )
                 OutlinedTextField(
-                    value = displayName,
-                    onValueChange = { newDisplayName -> displayName = newDisplayName },
+                    value = newName,
+                    onValueChange = { newName = it },
                     label = { Text("Display Name") },
                     modifier = Modifier.padding(16.dp)
                 )
                 OutlinedTextField(
-                    value = homeAddress,
-                    onValueChange = { newHomeAddress -> homeAddress = newHomeAddress },
+                    value = newHomeAddress,
+                    onValueChange = { newHomeAddress = it },
                     label = { Text("Home Address") },
                     modifier = Modifier.padding(16.dp)
                 )
@@ -87,6 +101,56 @@ class ProfileActivity {
                     fontSize = 16.sp,
                     modifier = Modifier.padding(16.dp)
                 )
+                if (newHomeAddress != homeAddress || newName != name) { // Show save button only if there are changes
+                     Button(
+                        onClick = {
+                            if (newName != name) { // Update display name if it's different
+                                user?.updateProfile(
+                                    UserProfileChangeRequest.Builder()
+                                        .setDisplayName(newName)
+                                        .build()
+                                )?.addOnCompleteListener { updateDisplayName ->
+                                    if (updateDisplayName.isSuccessful) {
+                                        name = newName
+                                        Toast.makeText(
+                                            context,
+                                            "Display name updated successfully",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    } else {
+                                        Toast.makeText(
+                                            context,
+                                            "Failed to update display name",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+                            }
+                            if (newHomeAddress != homeAddress) { // Update home address if it's different
+                                FirestoreHelper().setUserHomeAddress(user!!.uid, newHomeAddress)
+                                    .addOnCompleteListener { updateHomeAddress ->
+                                        if (updateHomeAddress.isSuccessful) {
+                                            homeAddress = newHomeAddress
+                                            Toast.makeText(
+                                                context,
+                                                "Home address updated successfully",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        } else {
+                                            Toast.makeText(
+                                                context,
+                                                "Failed to update home address",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                    }
+                            }
+                    },
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text("Save Changes")
+                    }
+                }
             }
         }
     }
