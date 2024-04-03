e52a12130be1555b655e1c86305e0bc144496b8a
Tmal1652 <103225901+Tmal1652@users.noreply.github.com>
2024-04-03 19:01:25 -0400

    Added Icons

--- app/src/main/java/com/protify/protifyapp/HomeActivity.kt
+++ app/src/main/java/com/protify/protifyapp/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.protify.protifyapp
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -20,6 +22,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
@@ -32,6 +36,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -85,6 +90,7 @@ class HomeActivity {
                                 fontSize = 20.sp,
                                 color = MaterialTheme.colors.onSurface
                             )
+                        Icon(Icons.Filled.AccountCircle, contentDescription = "Profile Icon")
                         Spacer(modifier = Modifier.height(32.dp))
                             Divider()
                         Spacer(modifier = Modifier.height(32.dp))
@@ -95,6 +101,7 @@ class HomeActivity {
                                 fontSize = 20.sp,
                                 color = MaterialTheme.colors.onSurface
                             )
+                        Icon(Icons.Filled.LocationOn, contentDescription = "Location Icon")
                         Spacer(modifier = Modifier.height(32.dp))
                             Divider()
                         Spacer(modifier = Modifier.height(32.dp))
@@ -104,6 +111,13 @@ class HomeActivity {
                             fontWeight = FontWeight.Bold,
                             fontSize = 20.sp,
                             color = MaterialTheme.colors.onSurface
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.book),
+                            contentDescription = "Recipe Book",
+                            modifier = Modifier
+                                .size(60.dp)
+                                .padding(16.dp)
                         )
                         Spacer(modifier = Modifier.height(32.dp))
                         Divider()
